30fe2b7dd5c78d7fa297b0e963a809b1983dfd23
unknown <borishan.gh@gmail.com>
2024-03-27 01:24:17 +0530

    fixed page

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css')
     eleventyConfig.addPassthroughCopy('./src/images')
+
+    eleventyConfig.addFilter("filterPostsByTag", function(posts, tag) {return posts.filter(post => post.data.tags.includes(tag));});
+
+    eleventyConfig.addFilter("sortContentsByIssueNo", function (contents) {
+        return contents.sort((a, b) => a.data.issueNo - b.data);
+    });
+
     return {
         dir: {
             input: "src",
